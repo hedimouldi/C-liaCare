dc1833545bc53cea6e277dbf98782642717ef2a4
hedimouldi <Hedi.Elmouldi@esprit.tn>
2026-06-15 22:04:00 +0100

    Fix Express production fallback route

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ app.post('/api/chat', async (req, res) => {
 if (isProduction) {
   const distPath = path.join(__dirname, 'dist');
   app.use(express.static(distPath));
-  app.get('*', (_req, res) => {
+  app.use((_req, res) => {
     res.sendFile(path.join(distPath, 'index.html'));
   });
 } else {
